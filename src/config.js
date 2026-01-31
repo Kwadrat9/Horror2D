a7a1ec0d50496cec0dac3d6cd92d57b54958c7e7
Kwadrat9 <128845521+Kwadrat9@users.noreply.github.com>
2026-01-31 22:08:20 +0100

    Add configuration file for game physics and assets

---
+++ src/config.js
@@ -0,0 +1,18 @@
+// Plik: src/config.js
+
+// Konfiguracja fizyki
+export const SPEED_MODIFIER = 0.6; 
+export const GRAVITY = 0.5 * SPEED_MODIFIER;
+export const PLAYER_SPEED = 4 * SPEED_MODIFIER; 
+export const JUMP_FORCE = -9 * SPEED_MODIFIER;
+
+// Lista obrazków do załadowania
+export const IMAGE_LIST = [
+    'player', 'tarantula', 'breather',
+    'bg_hol', 'bg_salon', 'bg_kuchnia', 'bg_sypialnia', 'bg_korytarz', 
+    'bg_strych', 'bg_poddasze', 'bg_sklad', 'bg_kotlownia', 'bg_bunkier',
+    'item_buty', 'item_klucz', 'item_noz', 'item_zapalniczka', 'item_dyktafon',
+    'item_maska', 'item_klucz_nastawny', 'item_lampa', 'item_lom', 'item_wegiel',
+    'item_rekawice', 'item_bomba', 'item_skrzynia', 'item_stol', 'item_piec',
+    'item_part1', 'item_part2', 'item_part3'
+];
